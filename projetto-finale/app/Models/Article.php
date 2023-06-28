b738a775d3e09f7d94fcdf3e95cbdb8a1d186041
fouzi <faouzi_ee@hotmail.it>
2023-06-28 17:26:49 +0200

    create' models e controllers and migration

---
+++ projetto-finale/app/Models/Article.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Article extends Model
+{
+    use HasFactory;
+    protected $fillable = [
+        'title','description','body','img','user_id','category_id'
+    ];
+}
---
+++ projetto-finale/resources/views/components/navbar.blade.php
@@ -0,0 +1,54 @@
+<div>
+
+    <!-- Navigation -->
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark static-top">
+        <div class="container">
+            <a class="navbar-brand" href="#">
+                <img src="https://placeholder.pics/svg/150x50/888888/EEE/Logo" alt="..." height="36">
+            </a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
+                data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false"
+                aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav ms-auto">
+                    <li class="nav-item">
+                        <a class="nav-link active" aria-current="page" href="{{ route('homepage') }}">Home</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Link</a>
+                    </li>
+                    <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button"
+                            data-bs-toggle="dropdown" aria-expanded="false">
+                            Log
+                        </a>
+                        <ul class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
+                        @guest
+                            <li><a class="dropdown-item" href="{{ route('login') }}">Login</a></li>
+                                <li><a class="dropdown-item" href="{{ route('register') }}">Register</a></li
+                        @endguest
+                        @auth
+                            <li class="nav-item">
+                                <a class="nav-item">{{ Auth::user()->name }}</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="dropdown-item" href="{{ route('logout') }}"
+                                onclick="eventDefault();document.getElementById('form-logout').submit();">logout</a>
+                                 <form method="POST" action="{{ route('logout') }}" style="display:  none" id="form-logout">
+                                @csrf
+                                </form>
+                            </li>
+
+                        @endauth
+
+
+                         </ul>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+
+</div>
